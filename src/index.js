b7480e36b7ae84f5a28ccba1c1bab1774fd5f756
Archit Maniyath <archit778@gmail.com>
2023-01-15 22:43:12 +0000

    env varibale to REACT_APP_API_URL

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 if(process.env.NODE_ENV ==="production"){
   console.log("inside production")
-  window.API_URL = process.env.React_App_API_ENDPOINT
+  window.API_URL = process.env.REACT_APP_API_URL
+  console.log("api url ",window.API_URL)
 }else{
-  window.API_URL = process.env.React_App_API_ENDPOINT
+  console.log("dev")
+  window.API_URL = process.env.REACT_APP_API_URL
+  console.log("api url ",window.API_URL)
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
